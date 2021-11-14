66403024ac5dc98e43a674103428fe9bcdeabdf4
ksmyc77 <41743479+ksmyc77@users.noreply.github.com>
2021-11-15 01:54:15 +0900

    Delete game directory

--- game/ScenarioElement.py
+++
@@ -1,4 +0,0 @@
-class ScenarioElement():
-    def __init__(self, sprite, crop_rect, coord_list):
-        self.surface = sprite.subsurface(crop_rect)
-        self.coord_list = coord_list
--- game/gameMain.py
+++
@@ -1,9 +0,0 @@
-"""
-Dino Runner Clone
-"""
-
-from gameRun import *
-
-#game running
-if __name__ == '__main__':
-    gameRun()
