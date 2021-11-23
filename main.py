89e7ee6ad2ad2657ecef5927d899dc6b3484322f
nevasca1125 <90556146+nevasca1125@users.noreply.github.com>
2021-11-24 05:08:42 +0900

    make GameRun class

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ game_state = 0
 if __name__ == '__main__':
     while game_state != -1:
         if game_state == 0:
-            game_state = game.gameRun()
+            game_state = game.GameRun.gameRun()
         elif game_state == 1:
             game_state = GameOver.overRun()
